065826b2249b75901ea0491186ff3470bc82f6af
devlopersumit <sumitsj2002@gmail.com>
2025-12-17 19:43:15 +0530

    Remove comments from user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const createUser = require('../controller/user.controller');
+const {
+    createUser,
+    getAllUsers,
+    getUserById,
+    getUserByXUserId,
+    updateUser,
+    updateLastPosted,
+    connectXAccount,
+    deleteUser,
+    getUserStats
+} = require('../controller/user.controller');
 
 const userRouter = express.Router();
 
 userRouter.post('/', createUser);
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id/stats', getUserStats);
+userRouter.get('/x/:xUserId', getUserByXUserId);
+userRouter.get('/:id', getUserById);
+userRouter.patch('/:id/last-posted', updateLastPosted);
+userRouter.patch('/:id/connect-x-account', connectXAccount);
+userRouter.put('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
 
 module.exports = userRouter;
